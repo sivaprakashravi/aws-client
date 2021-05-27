93be70503ccd3ef061acbfdc32f91917802de78b
Sivaprakasharavi Baskran <sivaprakashravi@gmail.com>
2021-05-27 21:24:57 +0530

    orders api integration

--- src/app/features/orders/orders.component.ts
+++ src/app/features/orders/orders.component.ts
@@ -6,13 +6,10 @@ import {
   FormControl,
   FormGroup
 } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { JobSchedulerService } from 'src/app/services/backend/job-scheduler.service';
-import { ProductService } from 'src/app/services/backend/product.service';
 import * as _ from 'lodash';
-import { DialogService } from 'src/app/services/dialog.service';
-import { AppService } from 'src/app/services/app.service';
+import * as moment from 'moment';
 import { OrdersService } from 'src/app/services/backend/orders.service';
+import { ToastService } from 'src/app/services/toast.service';
 
 @Component({
   selector: 'app-orders',
@@ -35,20 +32,18 @@ export class OrdersComponent implements OnInit {
   totalOrders;
   pages = 0;
   activePage = 1;
+  today = new Date();
   constructor(
-    private jobSchedulerService: JobSchedulerService,
-    private router: ActivatedRoute,
-    private dialog: DialogService,
     private ordersService: OrdersService,
-    private appService: AppService) {
+    private toast: ToastService) {
   }
 
   async ngOnInit() {
-    this.setFilter();
     await this.getStatuses();
     await this.getShops();
-    this.applyParams();
-    this.search();
+    this.setFilter();
+    // this.applyParams();
+    // this.search();
   }
 
   applyParams() {
@@ -57,19 +52,24 @@ export class OrdersComponent implements OnInit {
 
   setFilter() {
     this.filter = new FormGroup({
-      shop_id: new FormControl(''),
-      status: new FormControl(''),
-      from_date: new FormControl(new Date()),
-      to_date: new FormControl(new Date())
+      shop_id: new FormControl(this.shops[0]),
+      status: new FormControl(this.statuses[0]),
+      from_date: new FormControl(moment().subtract(3, 'days').format()),
+      to_date: new FormControl(moment().format())
     });
   }
 
   async search(pageNo?) {
-    const orders = await this.ordersService.orders(this.filter.value);
-    this.orders = orders;
-    // this.pages = Math.ceil(total / 25);
-    // this.totalProducts = total;
-    this.activePage = pageNo;
+    const filters = this.filter.value;
+    if (filters.from_date && filters.to_date && moment(filters.to_date).diff(filters.from_date, 'days') <= 3) {
+      const orders = await this.ordersService.orders(this.filter.value);
+      this.orders = orders;
+      // this.pages = Math.ceil(total / 25);
+      // this.totalProducts = total;
+      this.activePage = pageNo;
+    } else {
+      this.toast.createToast({message: 'Invalid date Range. Range should not be greater than 3'});
+    }
   }
 
   async getStatuses() {
